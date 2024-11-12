6472ef8eb8e007b252f7dd7d4d9ff56679c6cbf0
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-11-12 10:17:12 +0100

    basics

--- exercises/find.go
+++ exercises/find.go
@@ -1,8 +1,59 @@
 package exercises
 
-import "path/filepath"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 func Find() {
-	// "Usage: find [-p <start path>] -n <pattern> -t <file|dir|symlink>"
-	filepath.Walk("")
+	path := flag.String("p", "", "Start path")
+	name := flag.String("n", "", "Name to match")
+	fileType := flag.String("t", "", "Type to match (file, dir, symlink)")
+
+	flag.Parse()
+
+	if *name == "" || *fileType == "" {
+		flag.Usage()
+		return
+	}
+
+	if *path == "" {
+		*path = "."
+	}
+
+	if filepath.Walk(*path, onElement(*fileType, *name)) != nil {
+		log.Fatalf("Error reading file")
+		return
+	}
+
+}
+
+func onElement(fileType, name string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !strings.Contains(info.Name(), name) {
+			return nil
+		}
+		switch fileType {
+		case "file":
+			if info.Mode().IsRegular() {
+				fmt.Println(path)
+			}
+		case "dir":
+			if info.Mode().IsDir() {
+				fmt.Println(path)
+			}
+		case "symlink":
+			if info.Mode()&os.ModeSymlink == os.ModeSymlink {
+				fmt.Println(path)
+			}
+		}
+		return nil
+	}
 }
