c0853c889c782bf5214d4ec38aef6cb17f3a2935
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-11-12 14:57:44 +0100

    basics

--- exercises/db/database.go
+++ exercises/db/database.go
@@ -1,8 +1,11 @@
 package db
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"slices"
 	"sync"
 )
 
@@ -16,7 +19,7 @@ type Record struct {
 
 type Database struct {
 	file        *os.File
-	lock        sync.RWMutex
+	lock        sync.Mutex
 	records     []Record
 	idGenerator IdGenerator
 }
@@ -38,6 +41,47 @@ func (d *Database) saveState() {
 	if err != nil {
 		fmt.Println("Error saving state file")
 	}
+}
+
+func (d *Database) Insert(input interface{}) (*Record, error) {
+	bytes, err := ToBytes(input)
+	if err != nil {
+		return nil, err
+	}
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	offset, err := d.file.Seek(0, io.SeekEnd)
+	if err != nil {
+		return nil, err
+	}
+	length, err := d.file.WriteAt(bytes, offset)
+	if err != nil {
+		return nil, err
+	}
+	record := Record{d.idGenerator.next(), offset, int64(length)}
+	d.records = append(d.records, record)
+	d.saveState()
+	return &record, nil
+}
+
+func (d *Database) FindById(id int64, output interface{}) error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	idx := slices.IndexFunc(d.records, func(r Record) bool { return r.Id == id })
+	if idx != -1 {
+		record := d.records[idx]
+		buffer := make([]byte, record.Length)
+		_, err := d.file.ReadAt(buffer, record.Offset)
+		if err != nil {
+			return err
+		}
+		err = FromBytes(buffer, output)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("record not found")
 }
 
 func Db(filePath string) (*Database, error) {
@@ -63,13 +107,29 @@ func Db(filePath string) (*Database, error) {
 	}
 	generator := &Sequence{lastId}
 
-	db := &Database{file, sync.RWMutex{}, records, generator}
+	db := &Database{file, sync.Mutex{}, records, generator}
 	return db, nil
 }
 
+type User struct {
+	FirstName string
+	LastName  string
+	IsActive  bool
+}
+
 func Run() {
-	db, err := Db("users.db")
-	fmt.Println(err)
+	db, _ := Db("users.db")
 	defer db.Close()
-	db.saveState()
+	/*record, err := db.Insert(&User{"Jan", "Kowalski", true})
+	if err != nil {
+		fmt.Println("Error inserting user")
+	}
+	fmt.Println(record)*/
+
+	user := User{}
+	err := db.FindById(1, &user)
+	if err != nil {
+		fmt.Println("Error finding user")
+	}
+	fmt.Println("User: ", user)
 }
