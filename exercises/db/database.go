e2fd7abbc2a9021c67c28665cc68259933a2b7b2
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-11-12 15:43:53 +0100

    basics

--- exercises/db/database.go
+++ exercises/db/database.go
@@ -64,10 +64,14 @@ func (d *Database) Insert(input interface{}) (*Record, error) {
 	return &record, nil
 }
 
+func (d *Database) findRecordIndex(id int64) int {
+	return slices.IndexFunc(d.records, func(r Record) bool { return r.Id == id })
+}
+
 func (d *Database) FindById(id int64, output interface{}) error {
 	d.lock.Lock()
 	defer d.lock.Unlock()
-	idx := slices.IndexFunc(d.records, func(r Record) bool { return r.Id == id })
+	idx := d.findRecordIndex(id)
 	if idx != -1 {
 		record := d.records[idx]
 		buffer := make([]byte, record.Length)
@@ -82,6 +86,56 @@ func (d *Database) FindById(id int64, output interface{}) error {
 		return nil
 	}
 	return errors.New("record not found")
+}
+
+func (d *Database) DeleteById(id int64) error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	idx := d.findRecordIndex(id)
+	if idx != -1 {
+		d.records = append(d.records[:idx], d.records[idx+1:]...)
+		d.saveState()
+		return nil
+	}
+	return errors.New("record not found")
+}
+
+func (d *Database) UpdateById(id int64, input interface{}) error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	idx := d.findRecordIndex(id)
+	if idx != -1 {
+		bytes, err := ToBytes(input)
+		if err != nil {
+			return err
+		}
+		offset, err := d.file.Seek(0, io.SeekEnd)
+		if err != nil {
+			return err
+		}
+		length, err := d.file.WriteAt(bytes, offset)
+		if err != nil {
+			return err
+		}
+		d.records[idx].Offset = offset
+		d.records[idx].Length = int64(length)
+		d.saveState()
+		return nil
+	}
+	return errors.New("record not found")
+}
+
+func getMaxId(records []Record) int64 {
+	if len(records) == 0 {
+		return 0
+	}
+	maxId := records[0].Id
+	for _, record := range records {
+		if record.Id > maxId {
+			maxId = record.Id
+		}
+	}
+	return maxId
 }
 
 func Db(filePath string) (*Database, error) {
@@ -101,11 +155,7 @@ func Db(filePath string) (*Database, error) {
 		}
 	}
 
-	var lastId int64
-	if len(records) > 0 {
-		lastId = records[len(records)-1].Id
-	}
-	generator := &Sequence{lastId}
+	generator := &Sequence{getMaxId(records)}
 
 	db := &Database{file, sync.Mutex{}, records, generator}
 	return db, nil
@@ -120,16 +170,19 @@ type User struct {
 func Run() {
 	db, _ := Db("users.db")
 	defer db.Close()
+
 	/*record, err := db.Insert(&User{"Jan", "Kowalski", true})
 	if err != nil {
 		fmt.Println("Error inserting user")
 	}
 	fmt.Println(record)*/
 
+	_ = db.DeleteById(1)
+
+	//_ = db.UpdateById(1, &User{"Jan", "Nowak", false})
+
 	user := User{}
-	err := db.FindById(1, &user)
-	if err != nil {
-		fmt.Println("Error finding user")
+	if db.FindById(1, &user) == nil {
+		fmt.Println("User: ", user)
 	}
-	fmt.Println("User: ", user)
 }
